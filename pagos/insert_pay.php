424550e70289f90dcab4401847c7fc2ea3e1ba89
coders lab <50688804+coderslab01@users.noreply.github.com>
2019-05-24 13:16:00 -0500

    Update insert_pay.php

--- pagos/insert_pay.php
+++ pagos/insert_pay.php
@@ -6,7 +6,7 @@ $nombre_equipo=$_POST["nombre"];
 $horario=$_POST["fecha"];
 $cantidad=$_POST["cantidad"];
 //consulta
-$insertar="INSERT INTO pagos (id_equipos,nombre,fecha,cantidad) VALUES('$id_eq','$nombre_equipo','$horario','$cantidad')";
+$insertar="INSERT INTO pagos (id_equipo,nombre,fecha,cantidad) VALUES('$id_eq','$nombre_equipo','$horario','$cantidad')";
 
 $res=mysqli_query($conecta,$insertar);
 if(!$res){
